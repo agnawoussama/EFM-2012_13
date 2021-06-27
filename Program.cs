1066feb539168be99e72dea896834c5811acdd70
agnawoussama@gmail.com <agnawoussama@gmail.com>
2021-06-27 04:18:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFM_2012_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stagiaire s1 = new Stagiaire("karami", "loubna", "TSDM", 10, 10, 10); // 10
+            //Stagiaire s2 = new Stagiaire("Jamal", "Youssef", "TSDM", 18, 18, 18); // 18
+            //Stagiaire s3 = new Stagiaire("Ilham", "Fayrouz", "TSDM", 12, 13, 14); // 13         
+
+            int choice;
+            do
+            {
+                
+                Console.WriteLine("Entrez 1 pour Afficher: ");
+                Console.WriteLine("Entrez 2 pour Ajouter");
+                Console.WriteLine("Entrez 3 pour Rechercher");
+                Console.WriteLine("Entrez 4 pour Mofifier");
+                Console.WriteLine("Entrez 5 pour Supprimer");
+                Console.WriteLine("Entrez 6 pour Aficher la meilleure moyenne");
+                Console.WriteLine("Entrez -1 Quittez");
+
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1: Console.Clear(); Stagiaire.listDesStagiers.ForEach(Console.WriteLine); break;
+                    case 2: Ajouter(); break;
+                    case 3: Rechercher(); break;
+                    case 4: Modifier(); break;
+                    case 5: Supprimer();break;
+                    case 6: MeilleureMoyenne(); break;
+                }
+                
+            } while (choice != -1);
+                              
+        }
+   
+        static void Ajouter()
+        {
+            Console.Clear();
+            Stagiaire s = new Stagiaire();
+            Console.Write("Entree le nom: "); s.Nom = Console.ReadLine();
+            Console.Write("Entree le prenom: "); s.Prenom = Console.ReadLine();
+            Console.Write("Entree le filiere: "); s.Filiere = Console.ReadLine();
+            Console.Write("Entree la premiere note: "); s.Note1 = double.Parse(Console.ReadLine());
+            Console.Write("Entree la deuxieme note: "); s.Note2 = double.Parse(Console.ReadLine());
+            Console.Write("Entree la troisieme note: "); s.Note3 = double.Parse(Console.ReadLine());
+            Stagiaire.listDesStagiers.Add(s);
+        }
+
+        static void Rechercher()
+        {
+            Console.Clear();
+            Console.WriteLine("Entrez le matricule pour Rechercher: ");
+            int matr = int.Parse(Console.ReadLine());
+            foreach (Stagiaire s in Stagiaire.listDesStagiers)
+            {
+                if (s.Matricule == matr)
+                    Console.WriteLine(s.ToString());
+            }
+        }
+
+        static void Modifier()
+        {
+            Console.Clear();
+            Console.WriteLine("Entrez le matricule de staigiare pour Modifier: ");
+            int matr = int.Parse(Console.ReadLine());
+            foreach (Stagiaire s in Stagiaire.listDesStagiers)
+            {
+                if (s.Matricule == matr)
+                {
+                    Console.Write("Entree le nouveau nom: "); s.Nom = Console.ReadLine();
+                    Console.Write("Entree le nouveau prenom: "); s.Prenom = Console.ReadLine();
+                    Console.Write("Entree le nouveau filiere: "); s.Filiere = Console.ReadLine();
+                    Console.Write("Entree la nouveau premiere note: "); s.Note1 = double.Parse(Console.ReadLine());
+                    Console.Write("Entree la nouveau deuxieme note: "); s.Note2 = double.Parse(Console.ReadLine());
+                    Console.Write("Entree la nouveau troisieme note: "); s.Note3 = double.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        static void Supprimer()
+        {
+            Console.Clear();
+            Console.WriteLine("Entrez le matricule pour Supprimer: ");
+            int matr = int.Parse(Console.ReadLine());
+            foreach (Stagiaire s in Stagiaire.listDesStagiers.ToList())
+            {
+                if (s.Matricule == matr)
+                    Stagiaire.listDesStagiers.Remove(s);
+            }
+        }
+
+        static void MeilleureMoyenne()
+        {
+            Console.Clear();         
+            Stagiaire.listDesStagiers.Sort();
+            Stagiaire.listDesStagiers.Reverse();
+            Console.WriteLine($"La meilleure moyenne est: {Stagiaire.listDesStagiers[0].Calculer()} de Stagiare {Stagiaire.listDesStagiers[0].Nom} {Stagiaire.listDesStagiers[0].Prenom}");
+        }
+    }
+}
---
+++ Stagiaire.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFM_2012_13
+{
+    class Stagiaire : IComparable<Stagiaire>
+    {
+        //attributs
+        int matricule;
+        string nom;
+        string prenom;
+        string filiere;
+        Double note1;
+        Double note2;
+        Double note3;
+        public static int nombreStagieres;
+        public static List<Stagiaire> listDesStagiers = new List<Stagiaire>();
+
+        //proprietes
+        public int Matricule  { get { return matricule; } set { matricule = value; } }
+        public string Nom     { get { return nom;       } set { nom       = value; } }
+        public string Prenom  { get { return prenom;    } set { prenom    = value; } }
+        public string Filiere { get { return filiere;   } set { filiere   = value; } }
+        public Double Note1   { get { return note1;     } set { note1     = value; } }
+        public Double Note2   { get { return note2;     } set { note2     = value; } }
+        public Double Note3   { get { return note3;     } set { note3     = value; } }
+
+        //Constructeur sans parametrer
+        public Stagiaire()
+        {
+            nombreStagieres++;
+            matricule = nombreStagieres;
+        }
+
+        //Constructeur modifie
+        public Stagiaire(string nom, string prenom,string filiere)
+        {
+            nombreStagieres++;
+            matricule = nombreStagieres;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.filiere = filiere;
+        }
+
+        //Constructeur d'initialisation
+        public Stagiaire(string nom,string prenom,string filiere,Double note1,Double note2,Double note3)
+        {
+            nombreStagieres++;
+            matricule = nombreStagieres;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.filiere = filiere;
+            this.note1 = note1;
+            this.note2 = note2;
+            this.note3 = note3;
+        }
+
+        //Methode qui instance le compteur a zero
+        public void RAZ() { nombreStagieres = 0; }
+
+        //Methode Equals (par nom)
+        public override bool Equals(object obj)
+        {
+            Stagiaire s = obj as Stagiaire;
+            if (nom == s.nom) return true;
+            else return false;    
+        }
+        double moyenne;
+        //Methode qui calcule la moyenne generale d'etudiants
+        public double Calculer()
+        {
+            moyenne = (note1 + note2 + note3) / 3;
+            return moyenne; 
+        }
+
+        //ToString overriden
+        public override string ToString()
+        {
+            double m = Calculer();
+            return $"Matricule: {Matricule}\nNom: {Nom}\nPrenom: {Prenom}\nFiliere: {Filiere}\nNote1: {Note1}\nNote2: {Note2}\nNote3: {Note3}\nMoyenne {m}\n----------------";
+        }
+
+        //Implementation de l'interface IComparable
+        public int CompareTo(Stagiaire other)
+        {
+            double moy = Calculer();
+            if (moy > other.Calculer())
+                return 1;
+            else if (moy == other.Calculer())
+                return 0;
+            else return -1;
+        }
+
+        public static void SortDecroissant()
+        {
+            listDesStagiers.Sort();
+            listDesStagiers.Reverse();
+            listDesStagiers.ForEach(Console.WriteLine);
+        }
+
+        public static void SortCroissant()
+        {
+            listDesStagiers.Sort();
+            listDesStagiers.ForEach(Console.WriteLine);
+        }
+    }
+}
